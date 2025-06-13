07fa6ce36b10994d6bbe21c4733797ef13b37c3d
Ivansquark <squark@mail.ru>
2025-06-13 20:05:22 +0300

    error in stderr

--- desktop/minigrep/src/main.rs
+++ desktop/minigrep/src/main.rs
@@ -6,23 +6,15 @@ use minigrep::Config;
 fn main() {
     println!("minigrep");
     let args: Vec<String> = env::args().collect();
-    //let config = parse_config(&args);
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
 
-    //run(config).unwrap();
     if let Err(e) = minigrep::run(config) {
-        println!("Problem in read file: {e}");
+        eprintln!("Problem in read file: {e}");
         process::exit(1);
     }
-    //run(config).unwrap_or_else(|err| {
-    //    println!("Problem in read file: {err}");
-    //    process::exit(1);
-    //})
-
-    //dbg!(args);
 }
